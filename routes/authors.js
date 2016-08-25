878a038c970bd31053524f1441c7c4b37cff0b6d
Scott Wilkins <scottwilkins@Scotts-mac.local>
2016-08-25 09:27:15 -0600

    solution for now

--- routes/authors.js
+++ routes/authors.js
@@ -16,17 +16,31 @@ function Authors_Books() {
   return knex('authors_books');
 }
 
-
 router.get('/', function(req, res, next) {
-  // get all authors from Authors
-  // THEN for each author, go get all of their book ids from Authors_Books
-  // THEN go get all that author's books
-  // AND add the array of books to the author object as 'books'
-  // render the appropriate template
-  // pass an array of authors to the view using locals
-
-  // EXAMPLE: { first_name: 'Laura', last_name: 'Lou', bio: 'her bio', books: [ this should be all of her book objects ]}
+  Authors().then(function(authors){
+    Promise.all(authors.map(author=>{
+      return Authors_Books().where('author_id', author.id).pluck('book_id').then(function(bookIds){
+        return Books().whereIn('id', bookIds).then(function(books){
+          author.books=books;
+          return authors;
+        })
+      })
+    })
+  ).then(function(authors){
+      res.render('authors/index', {authors:authors[0]})
+  })
+})
 });
+
+//   // get all authors from Authors
+//   // THEN for each author, go get all of their book ids from Authors_Books
+//   // THEN go get all that author's books
+//   // AND add the array of books to the author object as 'books'
+//   // render the appropriate template
+//   // pass an array of authors to the view using locals
+//
+//   // EXAMPLE: { first_name: 'Laura', last_name: 'Lou', bio: 'her bio', books: [ this should be all of her book objects ]}
+
 
 router.get('/new', function(req, res, next) {
   Books().select().then(function (books) {
@@ -38,7 +52,16 @@ router.post('/', function (req, res, next) {
   var bookIds = req.body.book_ids.split(",");
   delete req.body.book_ids;
   Authors().returning('id').insert(req.body).then(function (id) {
-    helpers.insertIntoAuthorsBooks(bookIds, Authors_Books, id[0]).then(function () {
+    bookIds = bookIds.filter(function (id) {
+      return id !== '';
+    })
+    return Promise.all(bookIds.map(function (book_id) {
+      book_id = Number(book_id)
+      return Authors_Books().insert({
+        book_id: book_id,
+        author_id: id[0]
+      })
+    })).then(function () {
       res.redirect('/authors');
     })
   })
@@ -64,11 +87,21 @@ router.post('/:id/delete', function (req, res, next) {
 })
 
 router.get('/:id/edit', function (req, res, next) {
+
   // find the author in Authors
   // get all of the authors book_ids from Authors_Books
   // get all of the authors books from BOOKs
   // render the corresponding template
   // use locals to pass books and author to the view
+  Authors().where('id', req.params.id).first().then(function(author){
+      return Authors_Books().where('author_id', req.params.id).pluck('book_id').then(function(bookIds){
+        return Books().whereIn("id", bookIds).then(function(books){
+          return [author, books]
+        })
+     })
+  }).then(function(data){
+    res.render('authors/show.jade', {author:data[0], books:data[1]})
+  })
 })
 
 router.post('/:id', function (req, res, next) {
@@ -77,6 +110,7 @@ router.post('/:id', function (req, res, next) {
   Authors().returning('id').where('id', req.params.id).update(req.body).then(function (id) {
     id = id[0];
     helpers.insertIntoAuthorsBooks(bookIds, id).then(function () {
+      console.log(bookIds);
     res.redirect('/authors');
     });
   })
@@ -88,6 +122,15 @@ router.get('/:id', function (req, res, next) {
   // get all of the authors books from BOOKs
   // render the corresponding template
   // use locals to pass books and author to the view
+  Authors().where('id', req.params.id).first().then(function(author){
+      return Authors_Books().where('author_id', req.params.id).pluck('book_id').then(function(bookIds){
+        return Books().whereIn("id", bookIds).then(function(books){
+          return [author, books]
+        })
+     })
+  }).then(function(data){
+    res.render('authors/show.jade', {author:data[0], books:data[1]})
+  })
 })
 
 module.exports = router;
--- routes/books.js
+++ routes/books.js
@@ -16,8 +16,30 @@ function Authors() {
 }
 
 router.get('/', function(req, res, next) {
+
   // your code here
+  //   // get all books from Books
+  //   // THEN for each book, go get all of their author ids from Authors_Books
+  //   // THEN go get all that book's authors
+  //   // AND add the array of authors to the book object as 'authors'
+  //   // render the appropriate template
+  //   // pass an array of books to the view using locals
+
+  Books().then(function(books){
+    Promise.all(books.map(book=>{
+      return Authors_Books().where('book_id', book.id).pluck('author_id').then(function(authorIds){
+        return Authors().whereIn('id', authorIds).then(function(authors){
+          book.authors=authors;
+          return books;
+        })
+      })
+    })
+  ).then(function(books){
+      res.render('books/index', {books:books[0]})
+  })
+})
 });
+
 
 router.get('/new', function(req, res, next) {
   res.render('books/new');
@@ -39,7 +61,14 @@ router.post('/', function (req, res, next) {
 })
 
 router.get('/:id/delete', function(req, res, next) {
-  // your code here
+  Books().where('id', req.params.id).first().then(function (book) {
+    helpers.getBookAuthors(book).then(function (bookAuthors) {
+      console.log(bookAuthors);
+      Authors().select().then(function (authors) {
+        res.render('books/delete', {authors: authors, book: book });
+      })
+    })
+  })
 });
 
 router.post('/:id/delete', function(req, res, next) {
@@ -55,7 +84,14 @@ router.get('/:id/edit', function(req, res, next) {
 });
 
 router.get('/:id', function(req, res, next) {
-  // your code here 
+  // return book and array of authors that wrote it
+  Books().where('id', req.params.id).then(function(book){
+    Authors_Books().where('book_id', req.params.id).pluck('author_id').then(function(authIds){
+      Authors().whereIn('id', authIds).then(function(authors){
+        res.render('books/show', {book:book[0], authors:authors})
+      })
+    })
+  })
 });
 
 router.post('/:id', function(req, res, next) {
